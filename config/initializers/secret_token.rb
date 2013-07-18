52608025c3ddb83717fd26a2397b6460a5ac7516
jonathanamccann <jonathanamccann@gmail.com>
2013-07-17 21:58:45 -0500

    Created App-Theme needs adjustment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projects::Application.config.secret_key_base = '9cf166f1108b4be63f52f9c5bcf652e0bbc41210e5d11d7b4cabd6bcd6817f7c16e528b121e1bd7c3da320c08f7635fbe66bdd379f9711e1d1059de70f6a9572'
